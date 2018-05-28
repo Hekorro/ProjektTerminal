43957f63a2d52700c0f5f0966e349c5d113f4cda
Kacper <kacpercharko@gmail.com>
2018-05-28 22:24:22 +0200

    Test

---
+++ src/Center.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class Center {
+
+	Center(){}
+	
+	private LinkedList<Bank> bankList = new LinkedList<Bank>();
+	private LinkedList<Date> logDataList = new LinkedList<Date>();
+	private LinkedList<Firm> firmList = new LinkedList<Firm>();
+	
+	public void addBank(String name) {
+		Bank bank = new Bank(name)
+		}
+	public void addFirm(String name) {}
+	public void addToLogBase(Card card, double amount, Date date) {}
+	public void deleteBank(Bank bank) {}
+	public void deleteFirm(Firm firm) {}
+	public List<Bank> getBanks(){
+		return bankList;
+	}
+	public List<Date> getLogData(){
+		return logDataList;
+	}
+	//autorize do przerobienia
+	public boolean autorize(Card card, double amount) {
+		return true;
+	}
+	public boolean autorize(FirmAccount firmAccount,double amount) {
+		return true;
+	}
+	public void search(LogData logData, String string) {}
+	public void browseBank() {}
+	public void browseFirm() {}
+	
+}
